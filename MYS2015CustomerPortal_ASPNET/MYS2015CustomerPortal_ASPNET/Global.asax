01a75a51032d4cbbbd8fe45ace420c205958cd4b
Andrii Kornytskyi <akornitskiy@exoft.net>
2015-05-21 18:15:25 +0300

    Initial

---
+++ MYS2015CustomerPortal_ASPNET/MYS2015CustomerPortal_ASPNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MYS2015CustomerPortal_ASPNET.MvcApplication" Language="C#" %>
---
+++ MYS2015CustomerPortal_ASPNET/MYS2015CustomerPortal_ASPNET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MYS2015CustomerPortal_ASPNET.Startup))]
+namespace MYS2015CustomerPortal_ASPNET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
